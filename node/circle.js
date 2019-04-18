84d674a87987e912834b1992b9d22e7c7005fde3
kamal <kamal.pandey@tothenew.com>
2019-04-18 15:21:31 +0530

    Updadted assignment Module import and export

--- node/circle.js
+++ node/circle.js
@@ -1,6 +1,6 @@
 const num1 = 10;
 const num2 = 20;
 
-export function add(){return (num1+num2);}
-export function sub(){return (num2-num1);}
+exports.add=()=>{return (num1+num2);}
+exports.sub=()=>{return (num2-num1);}
 
--- node/index.js
+++ node/index.js
@@ -1,6 +1,7 @@
 
 const http = require('http');
 const fs = require('fs');
+const circle = require('./circle.js')
 
 // let about;
 // const index =fs.readFile('index.html', async (err, data) => {
@@ -9,24 +10,37 @@ const fs = require('fs');
 //     about = fs.readFileSync('about.html');
 // })
 
-const index = fs.readFileSync('index.html')
+const index = fs.readFileSync('index.html');
 const about = fs.readFileSync('about.html');
 const contact = fs.readFileSync('contact.html');
 
-const server = http.createServer(function (request, response){
-    if(request.url==='/') {
-        response.end(index);
-    }
-    else if(request.url==='/about') {
-        response.end(about);
-    }else if(request.url ==='/contact'){
-        response.end(contact)
-    }else {
-        response.writeHead(404);
-        response.end('Error 404 Page not Found');
-    }
-})
+const server = http.createServer(function(request, response) {
+  if (request.url==='/') {
+      response.writeHead(200, { 'Content-Type': 'text/html',
+          'Trailer': 'Content-MD5' });
+     response.write(index);
+     response.write(`Addition is ${circle.add()}
+                    Subtraction is ${circle.sub()}`);
+     response.end();
+  }
+  else if (request.url==='/about') {
+      response.writeHead(200, { 'Content-Type': 'text/html',
+          'Trailer': 'Content-MD5' });
+     response.write(about);
+     response.end();
+  }else if (request.url ==='/contact'){
+      response.writeHead(200, { 'Content-Type': 'text/html',
+          'Trailer': 'Content-MD5' });
+     response.write(contact);
+     response.end();
+  }else {
+      response.writeHead(404, { 'Content-Type': 'text/html',
+          'Trailer': 'Content-MD5' });
+     response.write('Error 404 Page not Found');
+     response.end();
+  }
+});
 
-server.listen(3000, (res)=>{
+server.listen(3000, (res)=> {
     console.log("App Running at port 3000");
 });
